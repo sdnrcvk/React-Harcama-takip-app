d37ba3e9ccc989a910b70edb37c5f3027128c19a
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-15 16:56:28 +0300

    password show/hide button

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,21 +1,48 @@
 import './Login.module.css'
-import { Button, Container, Typography, FormControl, FilledInput, InputLabel } from '@mui/material'
+import { Button, Container, Typography, FormControl, FilledInput, 
+  InputLabel, InputAdornment, IconButton } from '@mui/material'
+import { useState } from 'react'
+import {Visibility, VisibilityOff} from '@mui/icons-material'
 
 export default function Login() {
+  const [values,setValues]=useState({
+    email:"",
+    password:"",
+    showPassword:"false"
+  })
+
+
+  const handleChange=(prop)=>(event)=>{
+    setValues({...values,[prop]:event.target.value})
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(values);
+  }
+
+  const handleClickShowPassword=()=>{
+    setValues({...values,showPassword:!values.showPassword})
+  }
+
   return (
     <Container>
-      <form>
+      <form onSubmit={handleSubmit}> 
         <Typography sx={{mt:15, ml:5, fontWeight:"bold"}} variant="h4"
         color="darkslateblue">
             Giriş Yap
         </Typography>
         <FormControl fullWidth sx={{mt:5}}>
           <InputLabel htmlFor="email">Email</InputLabel>
-          <FilledInput id="email" label="Email"/>
+          <FilledInput value={values.email} onChange={handleChange("email")} id="email" label="Email"/>
         </FormControl>
         <FormControl fullWidth sx={{my:5}}>
           <InputLabel htmlFor="password">Şifre</InputLabel>
-          <FilledInput id="password" label="Password"/>
+          <FilledInput type={values.showPassword ? "text" : "password"} value={values.password} onChange={handleChange("password")}
+           id="password" label="Password" endAdornment={
+           <InputAdornment position='end'>
+              <IconButton aria-label='Toggle Password' onClick={handleClickShowPassword}>{values.showPassword ? <VisibilityOff/> : <Visibility/>}</IconButton>
+           </InputAdornment>}/>
         </FormControl>
         <Button variant='outlined' type='submit' color='info' size='large' sx={{mt:5}}>GİRİŞ</Button>
       </form>
